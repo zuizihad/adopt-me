1c111f4dabe8347f6b0d78a4315009a06feb7e4a
zuizihad <ahmedxihad@gmail.com>
2020-05-09 13:02:32 +0600

    reformatting the pet component

--- dist/App.d36a57b6.js
+++ dist/App.d36a57b6.js
@@ -28300,14 +28300,28 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 const Pet = ({
   name,
   animal,
-  breed
+  breed,
+  media,
+  location,
+  id
 }) => {
-  //   return React.createElement("div", {}, [
-  //     React.createElement("h1", {}, name),
-  //     React.createElement("h2", {}, animal),
-  //     React.createElement("h2", {}, breed),
-  //   ]);
-  return /*#__PURE__*/_react.default.createElement("div", null, /*#__PURE__*/_react.default.createElement("h1", null, name), /*#__PURE__*/_react.default.createElement("h2", null, animal), /*#__PURE__*/_react.default.createElement("h2", null, breed));
+  let hero = "http://placecorgi.com/300/300";
+
+  if (media.length) {
+    hero = media[0].small;
+  }
+
+  return /*#__PURE__*/_react.default.createElement("a", {
+    href: `/details/${id}`,
+    className: "pet"
+  }, /*#__PURE__*/_react.default.createElement("div", {
+    className: "image-container"
+  }, /*#__PURE__*/_react.default.createElement("img", {
+    src: hero,
+    alt: name
+  })), /*#__PURE__*/_react.default.createElement("div", {
+    className: "info"
+  }, /*#__PURE__*/_react.default.createElement("h1", null, name), /*#__PURE__*/_react.default.createElement("h2", null, `${animal} - ${breed} -${location}`)));
 };
 
 var _default = Pet;
