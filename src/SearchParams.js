0300b731777231ad0100e206da81de7e70cd3aa7
zuizihad <ahmedxihad@gmail.com>
2020-05-09 09:57:28 +0600

    searchParams component responsive

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SearchParams = () => {
-  const location = "Seattle WA";
+  const [location, setLocation] = useState("Seattle WA");
   return (
     <div className="search-params">
       <form>
         <label htmlFor="location">
           location
-          <input id="location" value={location} placeholder="Location" />
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(event) => setLocation(event.target.value)}
+          />
         </label>
         <button>Submit</button>
       </form>
