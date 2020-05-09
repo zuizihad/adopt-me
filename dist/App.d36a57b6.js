f43ade348cd3a400011be3427f269e9816fa78dc
zuizihad <ahmedxihad@gmail.com>
2020-05-10 02:11:01 +0600

    configure babel for parcel

--- dist/App.d36a57b6.js
+++ dist/App.d36a57b6.js
@@ -32813,12 +32813,15 @@ function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return 
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 class Details extends _react.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+  constructor(...args) {
+    super(...args);
+
+    _defineProperty(this, "state", {
       loading: true
-    };
+    });
   }
 
   componentDidMount() {
